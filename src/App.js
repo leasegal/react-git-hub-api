a7e0c43f19c113b7b4cb744835d98f275911614e
Avi Cohen <avic@insighting.io>
2022-06-06 22:01:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Search from "./components/Search/Search";
+import Button from "./components/Button/Button";
 
 function App() {
+  const [gitUser, setGitUser] = useState("");
+  const [searchedUser, setSearchedUser] = useState("");
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+console.log(user);
+  useEffect(() => {
+    try {
+      async function fetchData() {
+        const githubApiUrl = `https://api.github.com/users/${searchedUser}`;
+        // npm i axios
+        if (gitUser === "") return;
+        const { data } = await axios.get(githubApiUrl);
+        const { avatar_url, created_at, login, public_repos } = data;
+        setUser({ avatar_url, created_at, login, public_repos });
+
+        console.log(avatar_url, created_at, login, public_repos);
+      }
+      fetchData();
+    } catch (e) {
+      console.log(e);
+    }
+  }, [searchedUser]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search setGitUser={setGitUser} gitUser={gitUser} />
+      <Button
+        text={"Search"}
+        clickEvent={() => {
+          console.log("search");
+          setSearchedUser(gitUser);
+        }}
+      />
+      <Button text={"Reset"} clickEvent={() => console.log("Reset")} />
     </div>
   );
 }
